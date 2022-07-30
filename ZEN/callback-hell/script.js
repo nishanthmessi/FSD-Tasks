8fc60cb369de0e5918a74068774ce1988b3133a1
Nishanth <52668520+nishanthmessi@users.noreply.github.com>
2022-07-30 12:14:59 +0530

    Add files via upload

---
+++ ZEN/callback-hell/script.js
@@ -0,0 +1,34 @@
+let count=10
+setTimeout(function(){
+    document.getElementById("display").innerHTML = count--;
+    setTimeout(function(){
+        document.getElementById("display").innerHTML = count--;
+        setTimeout(function(){
+            document.getElementById("display").innerHTML = count--;
+            setTimeout(function(){
+                document.getElementById("display").innerHTML = count--;
+                setTimeout(function(){
+                    document.getElementById("display").innerHTML = count--;
+                    setTimeout(function(){
+                        document.getElementById("display").innerHTML = count--;
+                        setTimeout(function(){
+                            document.getElementById("display").innerHTML = count--;
+                            setTimeout(function(){
+                                document.getElementById("display").innerHTML = count--;
+                                setTimeout(function(){
+                                    document.getElementById("display").innerHTML = count--;
+                                    setTimeout(function(){
+                                        document.getElementById("display").innerHTML = count--;
+                                        setTimeout(function(){
+                                            document.getElementById("display").innerHTML = "Happy Independence Day!";
+                                        },1000);
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000);
